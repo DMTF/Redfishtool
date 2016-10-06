bac6674760c0cf4ec78de387c8f3bd28dff1d81f
Paul Vancil <paul_vancil@dell.com>
2016-10-06 12:26:42 -0500

    correcting License comment in .py files to point to local Redfishtool repo license

--- redfishtool.py
+++ redfishtool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # Copyright Notice:
 # Copyright 2016 Distributed Management Task Force, Inc. All rights reserved.
-# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Tools/LICENSE.md
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfishtool/LICENSE.md
 
 # redfishtool:  redfishtool.py  ver 0.9.1
 #
