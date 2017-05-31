5dc4941d6a46a2d0b07a5b701f117a7607d4519b
Bill Dodd <billdodd@gmail.com>
2017-05-31 13:34:01 -0500

    changed email address in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='redfishtool',
       version='1.0.0',
       description='Redfishtool package and command-line client',
       author='DMTF',
-      author_email='DMTF@DMTF.com',
+      author_email='dmtf@dmtf.org',
       license='BSD 3-clause "New" or "Revised License"',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
