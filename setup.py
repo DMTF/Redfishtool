a7130dd617866434ed962a83c3171c3e0009bbd7
Bill Dodd <billdodd@gmail.com>
2017-05-31 12:44:27 -0500

    updated setup.py for initial 1.0.0 PyPI submission

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
 
 setup(name='redfishtool',
-      version='0.9.0',
+      version='1.0.0',
       description='Redfishtool package and command-line client',
       author='DMTF',
       author_email='DMTF@DMTF.com',
       license='BSD 3-clause "New" or "Revised License"',
       classifiers=[
-          'Development Status :: 3 - Alpha',
+          'Development Status :: 5 - Production/Stable',
           'License :: OSI Approved :: BSD License',
           'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Communications'
       ],
       keywords='Redfish',
-      url='https://github.com/billdodd/Redfishtool',
-      download_url='https://github.com/billdodd/Redfishtool/archive/0.9.0.tar.gz',
+      url='https://github.com/DMTF/Redfishtool',
+      download_url='https://github.com/DMTF/Redfishtool/archive/1.0.0.tar.gz',
       packages=['redfishtool'],
       scripts=['redfishtool.py'],
       install_requires=['requests']
