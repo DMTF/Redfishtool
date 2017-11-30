fe73f7b86fa32c11d26cdc94d8050cfa83d19a40
Mike Raineri <mraineri@gmail.com>
2017-11-30 18:00:16 -0500

    Comment fix

--- redfishtool/raw.py
+++ redfishtool/raw.py
@@ -1,4 +1,4 @@
-# Copyrighfalset Notice:
+# Copyright Notice:
 # Copyright 2016 Distributed Management Task Force, Inc. All rights reserved.
 # License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfishtool/LICENSE.md
 
