633f6be81cf1cb44b41002fdd175d6c3f2b6d577
Bill Dodd <billdodd@gmail.com>
2020-07-20 11:42:10 -0500

    update copyright date on cnanged files

--- scripts/redfishtool.py
+++ scripts/redfishtool.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Copyright Notice:
-# Copyright 2016 DMTF. All rights reserved.
+# Copyright 2016, 2020 DMTF. All rights reserved.
 # License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfishtool/blob/master/LICENSE.md
 
 # redfishtool:  scripts/redfishtool.py
